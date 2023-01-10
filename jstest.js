a8e95b2b8d56c34f9a4b6fef838e89cb7aa7942b
Harukun19 <119783629+Hal-kun19@users.noreply.github.com>
2023-01-10 21:02:06 +0900

    Update jstest.js

--- jstest.js
+++ jstest.js
@@ -1,9 +1,9 @@
 function clickingButton() {
-  msg.innerText = getTbox.value
+  msg.innerText = tbox.value
 }
 
-let getTbox = document.getElementById('tbox')
-let getMsg = document.getElemenyById('msg')
+let tbox = document.getElementById('tbox')
+let msg = document.getElemenyById('msg')
 let checkButton = document.getElementById('button')
 
 checkButton.addEventListener('click', clickingButton)
