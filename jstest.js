0e433176757ef58f9bb6ac31798a11e8d5e1c831
Harukun19 <119783629+Hal-kun19@users.noreply.github.com>
2023-01-25 19:40:45 +0900

    Update jstest.js

--- jstest.js
+++ jstest.js
@@ -1,9 +1,9 @@
 function clickingButton() {
   msg.innerText = tbox.value
-}
+};
 
-let tbox = document.getElementById('tbox')
-let msg = document.getElemenyById('msg')
-let checkButton = document.getElementById('button')
+let tbox = document.getElementById('tbox');
+let msg = document.getElemenyById('msg');
+let checkButton = document.getElementById('button');
 
-checkButton.addEventListener('click', clickingButton)
+checkButton.addEventListener('click', clickingButton);
